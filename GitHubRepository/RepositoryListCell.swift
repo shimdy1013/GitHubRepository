df895153dab6f7cc4d8b9d56bcff1230ffff9a20
shimdy1013 <shimdy1013@naver.com>
2023-05-13 00:12:19 +0900

    기본 UI 완성

---
+++ GitHubRepository/RepositoryListCell.swift
@@ -0,0 +1,8 @@
+//
+//  RepositoryListCell.swift
+//  GitHubRepository
+//
+//  Created by 심두용 on 2023/05/12.
+//
+
+import Foundation
---
+++ GitHubRepository/RepositoryListViewController.swift
@@ -0,0 +1,8 @@
+//
+//  RepositoryListViewController.swift
+//  GitHubRepository
+//
+//  Created by 심두용 on 2023/05/12.
+//
+
+import Foundation
