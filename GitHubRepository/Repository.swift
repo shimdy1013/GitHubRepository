b209aba694529bda1c59249cf56a05ab5e4e44af
shimdy1013 <shimdy1013@naver.com>
2023-05-17 03:23:21 +0900

    didSelectRowAt 추가

--- GitHubRepository/Repository.swift
+++ GitHubRepository/Repository.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+struct Repository: Decodable {
+    let id: Int
+    let name: String
+    let description: String
+    let stargazersCount: Int
+    let language: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, description, language
+        case stargazersCount = "stargazers_count"
+    }
+}
