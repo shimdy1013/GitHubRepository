a727a241dbbb5eac0382e34cff2f797801fbf3e6
shimdy1013 <shimdy1013@naver.com>
2023-08-21 21:57:41 +0900

    MVVM 리팩토링중 에러나서 ViewModel만 남겨놓고 복구

---
+++ GitHubRepository/Presentation/RepoSearch/SubComponents/SearchListView/SearchListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SearchListViewModel.swift
+//  GitHubRepository
+//
+//  Created by 심두용 on 2023/08/21.
+//
+
+import Foundation
---
+++ GitHubRepository/Presentation/RepoSearch/SubComponents/SearchListView/SubComponents/FilterView/FilterViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FilterViewModel.swift
+//  GitHubRepository
+//
+//  Created by 심두용 on 2023/08/21.
+//
+
+import RxSwift
+import RxCocoa
+
+struct FilterViewModel {
+    
+}
